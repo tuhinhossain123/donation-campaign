7c2d53a710edadf756629f6d7098f93c43c4b8f5
Ariyan-Rahman-Anas <mohammadariyanarahmananas.com>
2023-09-26 10:50:19 +0600

    statistics donation added

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,11 +1,40 @@
 
+import { useLoaderData } from 'react-router-dom';
+import { Cell, Legend, Pie, PieChart, Tooltip } from 'recharts';
 
 const Statistics = () => {
-    return (
-        <div>
-            Statistics
-        </div>
-    );
+  const categories = useLoaderData();
+
+  const donationData = [
+    {
+      name: 'totalDonation',
+      donation: categories?.length,
+    },
+    {
+      name: 'yourDonation',
+      donation: JSON.parse(localStorage.getItem('donations'))?.length,
+    },
+  ];
+
+  return (
+    <div className="flex justify-center items-center">
+      <PieChart height={200} width={500}>
+        <Pie
+          data={donationData}
+          dataKey="donation"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          label={(val) => (val.percent * 100).toFixed(2) + '%'}>
+          <Cell key="donation" fill="#FF444A" />
+          <Cell key="donation" fill="#00C49F" />
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </div>
+  );
 };
 
 export default Statistics;
--- src/componentes/Header/Banner/Banner.jsx
+++ src/componentes/Header/Banner/Banner.jsx
@@ -1,13 +1,13 @@
 const Banner = () => {
   return (
-    <div className="bg-cover h-screen">
+    <div className="h-screen mb-10">
       <div
-        className="hero min-h-screen"
+        className="hero cover w-full min-h-screen"
         style={{
           backgroundImage: "url(../../../../../../public/Resources/banner.jpg)",
         }}
       >
-        <div className="hero-overlay bg-white bg-opacity-90"></div>
+        <div className="hero-overlay bg-white bg-opacity-95"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="">
             <h1 className="mb-10 text-5xl font-bold text-[#0B0B0B]">
@@ -16,10 +16,10 @@ const Banner = () => {
             <div className="flex justify-center items-center">
               <input
                 type="text"
-                placeholder="Search here"
-                className="rounded-l-lg border px-4 py-1"
+                placeholder="Search here...."
+                className="rounded-l-lg border px-4 py-2"
               />
-              <button className="border bg-[#FF444A] px-2 rounded-r-lg text-white py-1 font-medium">
+              <button className="border bg-[#FF444A] px-2 rounded-r-lg text-white py-2 font-medium">
                 Search
               </button>
             </div>
